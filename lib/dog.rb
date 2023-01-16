4416b1536a35abc2debde9b098e0d1916cbf7899
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-01-16 13:21:27 +1100

    Done with assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+    def name=(dogs_name)
+        @this_dogs_name = dogs_name
+    end
 
+    def name
+       @this_dogs_name
+    end
 end
+
+doggo = Dog.new
+
+doggo.name = 'Bob'
+
+puts doggo.name
